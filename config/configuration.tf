cb3c5b8d914cca0bbba9a5bb949b1db75361d221
Abhinav Choudhary <choudhary.ab@northeastern.edu>
2024-02-15 15:26:50 -0500

    updated next hop to internet gateway

--- config/configuration.tf
+++ config/configuration.tf
@@ -30,44 +30,11 @@ resource "google_compute_subnetwork" "db_subnet" {
   project       = var.project
 }
 
-resource "google_compute_health_check" "health_check" {
-  provider           = google-beta
-  name               = var.health_check_name
-  project            = var.project
-  check_interval_sec = var.health_check_interval
-  timeout_sec        = var.health_check_timeout
-
-  tcp_health_check {
-    port = var.health_check_port
-  }
-}
-
-resource "google_compute_region_backend_service" "backend" {
-  provider      = google-beta
-  name          = var.backend_name
-  project       = var.project
-  region        = var.region
-  health_checks = [google_compute_health_check.health_check.id]
-}
-
-# Adding a forwarding rule for the webapp route
-resource "google_compute_forwarding_rule" "vpc_forwarding_rule" {
-  provider = google
-  name     = var.forwarding_rule_name
-  region   = var.region
-
-  load_balancing_scheme = var.forwarding_load_balancing_scheme
-  all_ports             = var.forwarding_all_ports
-  network               = google_compute_network.vpc.name
-  subnetwork            = google_compute_subnetwork.webapp_subnet.name
-  backend_service       = google_compute_region_backend_service.backend.id 
-}
-
 # Create a route for webapp subnet
 resource "google_compute_route" "webapp_route" {
   provider     = google
   name         = var.webapp_route_name
   dest_range   = var.destination_range
-  network      = google_compute_network.vpc.name
-  next_hop_ilb = google_compute_forwarding_rule.vpc_forwarding_rule.ip_address
+  network      = google_compute_network.vpc.self_link
+  next_hop_gateway = var.internet_gateway
 }

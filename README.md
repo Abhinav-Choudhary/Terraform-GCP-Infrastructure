1816d0345d9d14a9f983281d2b74606a9f054e15
Abhinav Choudhary <choudhary.ab@northeastern.edu>
2024-02-14 19:40:19 -0500

    updated readme, removed hardcoded values

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Repository for course CSYE 6225 Network Structures and Cloud Computing offered b
 # Enabled Api's
 Following Google Cloud Api's have been enabled for this project:
 - Compute Engine API
+
+# How to run the application
+- Install Gcloud cli and Terraform
+- Run below command to set up google cloud credentials on local device
+  - gcloud auth application-default login
+- If you wish to create multiple vpc's at once or provide custom names for network, subnets, routes, etc. modify "main.tf" file.
+- Navigate to project repository on command line and run below Terraform commands
+  - Initialize the repository: terraform init
+  - Perform validation checks: terraform validate
+  - Create execution plan: terraform plan
+  - Apply changes mentioned in execution plan: terraform apply
--- config/variables.tf
+++ config/variables.tf
@@ -16,6 +16,24 @@ variable "network_name" {
     default = "abhinav-vpc"
 }
 
+variable "routing_mode" {
+  type = string
+  description = "Set routing mode to global or regional"
+  default = "REGIONAL"
+}
+
+variable "auto_create_subnet" {
+  type = bool
+  description = "Specify whether default subnets should be autocreated"
+  default = false
+}
+
+variable "delete_default_routes" {
+  type = bool
+  description = "Specify whether default routes should be created"
+  default = false
+}
+
 variable "ip_cidr_range_webapp" {
     type = string
     description = "IP CIDR range for subnet webapp"
@@ -26,6 +44,12 @@ variable "ip_cidr_range_db" {
     type = string
     description = "IP CIDR range for subnet db"
     default = "10.0.2.0/24"
+}
+
+variable "destination_range" {
+  type = string
+  description = "The destination range of outgoing packets that this route applies to. Only IPv4 is supported."
+  default = "0.0.0.0/0"
 }
 
 variable "webapp_subnet_name" {
@@ -52,6 +76,24 @@ variable "health_check_name" {
   default = "proxy-health-check"
 }
 
+variable "health_check_interval" {
+  type = number
+  description = "Set how often (in seconds) to send a health check"
+  default = 1
+}
+
+variable "health_check_timeout" {
+  type = number
+  description = "Set how long (in seconds) to wait before claiming failure."
+  default = 1
+}
+
+variable "health_check_port" {
+  type = string
+  description = "The TCP port number for the TCP health check request."
+  default = "80"
+}
+
 variable "backend_name" {
   type = string
   description = "Name for backend service"
@@ -62,4 +104,16 @@ variable "forwarding_rule_name" {
   type = string
   description = "Name for forwarding rule"
   default = "compute-forwarding-rule"
+}
+
+variable "forwarding_load_balancing_scheme" {
+  type = string
+  description = "Specifies the forwarding rule type"
+  default = "INTERNAL"
+}
+
+variable "forwarding_all_ports" {
+  type = bool
+  description = "Set the value of all ports field for forwarding rule"
+  default = true
 }

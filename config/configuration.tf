c7402335c84c4793021a9c99cb82edf1761a5ba6
Abhinav Choudhary <choudhary.ab@northeastern.edu>
2024-02-21 17:27:40 -0500

    added firewall and instance resources

--- config/configuration.tf
+++ config/configuration.tf
@@ -5,11 +5,11 @@ provider "google" {
 
 # Create the network
 resource "google_compute_network" "vpc" {
-  name                              = var.network_name
-  auto_create_subnetworks           = var.auto_create_subnet
-  routing_mode                      = var.routing_mode
-  project                           = var.project
-  delete_default_routes_on_create   = var.delete_default_routes
+  name                            = var.network_name
+  auto_create_subnetworks         = var.auto_create_subnet
+  routing_mode                    = var.routing_mode
+  project                         = var.project
+  delete_default_routes_on_create = var.delete_default_routes
 }
 
 # Create webapp subnet
@@ -32,9 +32,50 @@ resource "google_compute_subnetwork" "db_subnet" {
 
 # Create a route for webapp subnet
 resource "google_compute_route" "webapp_route" {
-  provider     = google
-  name         = var.webapp_route_name
-  dest_range   = var.destination_range
-  network      = google_compute_network.vpc.self_link
+  provider         = google
+  name             = var.webapp_route_name
+  dest_range       = var.destination_range
+  network          = google_compute_network.vpc.self_link
   next_hop_gateway = var.internet_gateway
+  tags             = var.tags
+  depends_on       = [google_compute_subnetwork.webapp_subnet]
+}
+
+resource "google_compute_firewall" "default" {
+  name    = var.firewall_name
+  network = google_compute_network.vpc.name
+  allow {
+    protocol = var.ping_protocol
+  }
+  allow {
+    protocol = var.http_protocol
+    ports    = var.firewall_tcp_allow_ports
+  }
+  source_tags = var.tags
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
+}
+
+resource "google_compute_instance" "webapp_instance" {
+  name         = "csye6225-instance"
+  machine_type = "e2-small"
+  zone         = "us-east1-b"
+  tags = var.tags
+
+  boot_disk {
+    initialize_params {
+      image = "csye6225-app-image"
+      size  = 100
+      type  = "pd-balanced"
+    }
+  }
+  network_interface {
+    subnetwork = google_compute_subnetwork.webapp_subnet.self_link
+    access_config {
+    nat_ip = google_compute_address.static.address
+  }
+  }
 }
--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
 module "vpc1" {
   source = "./config"
-}
-
-module "vpc2" {
-  source      = "./config"
-  network_name = "abhinav-vpc2"
-  webapp_subnet_name = "webapp2"
-  db_subnet_name = "db2"
-  webapp_route_name = "webapp-route-2"
-  ip_cidr_range_webapp = "10.1.1.0/24"
-  ip_cidr_range_db = "10.1.2.0/24"
+  # project = "csye6225-abhinav"
 }
